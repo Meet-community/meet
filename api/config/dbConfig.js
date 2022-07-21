50dd71b0f25e0b5549c2eb7e280988efb6d87c71
Serhii Kirichenko <91411692+SerhiiKirik@users.noreply.github.com>
2022-07-22 01:16:58 +0300

    added seed & sequelizerc (#17)
    Co-authored-by: Serhii Kirichenko <kirik@MacBook-Pro-Serhii.local>

---
+++ api/config/dbConfig.js
@@ -0,0 +1,30 @@
+const dotenv = require('dotenv');
+dotenv.config({ path: '.env' });
+const database = process.env.DB_NAME;
+const username = process.env.DB_USERNAME;
+const host = process.env.DB_HOST;
+const port = Number(process.env.DB_PORT);
+const password = process.env.DB_PASSWORD;
+
+const dbConfig = {
+  database,
+  username,
+  password,
+  host,
+  port,
+  dialect: 'postgres',
+  protocol: 'postgres',
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
+    },
+  },
+  logging: false,
+}
+
+module.exports = {
+  development: {
+    ...dbConfig,
+  }
+}
