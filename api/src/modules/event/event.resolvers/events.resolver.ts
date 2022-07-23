33ed19d77ff290ec1c1ab46fdb4337b2d61a0e88
Ihor Karpyn <63306833+Ihor-Karpyn@users.noreply.github.com>
2022-07-23 16:22:28 +0300

    Added email service (#29)

--- api/src/modules/event/event.resolvers/events.resolver.ts
+++ api/src/modules/event/event.resolvers/events.resolver.ts
@@ -1,5 +1,8 @@
 import { Ctx } from '../../../../server/typedefs';
 import { EventModel } from '../../../models/EventModel';
+import {
+  sendEmailConfirm
+} from '../../../services/emailService/templates/emailConfirm/sendEmailConfirm';
 
 export const eventsResolver = async (_, __, ctx: Ctx ): Promise<EventModel[]> => {
   return ctx.models.EventModel.findAll({
