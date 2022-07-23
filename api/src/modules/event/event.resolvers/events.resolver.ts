86f731698782ce8726d2f1b0052c525ef12d290f
Ihor Karpyn <igor82200@gmail.com>
2022-07-23 23:31:41 +0300

    remove-log

--- api/src/modules/event/event.resolvers/events.resolver.ts
+++ api/src/modules/event/event.resolvers/events.resolver.ts
@@ -2,10 +2,6 @@ import { Ctx } from '../../../../server/typedefs';
 import { EventModel } from '../../../models/EventModel';
 
 export const eventsResolver = async (_, __, ctx: Ctx ): Promise<EventModel[]> => {
-  console.log('----------------auth----------')
-  console.log(ctx.authUser);
-  console.log('----------------auth----------')
-
   return ctx.models.EventModel.findAll({
     order: [['startAt', 'ASC']],
     raw: true,
