8cc76e42b02b0a6b3047797f3817064225083d47
Serhii Kirichenko <kirik@MacBook-Pro-Serhii.local>
2022-07-24 18:38:44 +0300

    redesign-sign

--- frontend/src/components/SignIn/SignIn.tsx
+++ frontend/src/components/SignIn/SignIn.tsx
@@ -1,5 +1,6 @@
-import React, { FC, useState } from 'react';
+import React, { FC, useCallback, useEffect, useMemo, useState } from 'react';
 import {
+  useAuthUserQuery,
   useSignInMutation
 } from '../../controllers/graphql/generated';
 import Avatar from '@mui/material/Avatar';
@@ -13,18 +14,14 @@ import Grid from '@mui/material/Grid';
 import Box from '@mui/material/Box';
 import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
 import Typography from '@mui/material/Typography';
-import Container from '@mui/material/Container';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
-import { useApolloClient } from '@apollo/client';
 import { useRouter } from 'next/router';
+import { Paper } from '@mui/material';
 
 function Copyright(props: any) {
   return (
     <Typography variant="body2" color="text.secondary" align="center" {...props}>
-      {'Copyright © '}
-      <Link color="inherit" href="https://mui.com/">
-        Your Website
-      </Link>{' '}
+      {'Copyright © Meet '}
       {new Date().getFullYear()}
       {'.'}
     </Typography>
@@ -36,92 +33,159 @@ const theme = createTheme();
 export const SignIn: FC = React.memo(() => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
-  const [error, setError] = useState('');
+  const [passwordError, setPasswordError] = useState<string | null>(null);
+  const [emailError, setEmailError] = useState<string | null>(null);
+
+  const { data: authUserData } = useAuthUserQuery({
+    fetchPolicy: 'cache-and-network',
+  });
+
+  const authUser = useMemo(() => (authUserData?.authUser
+    ? authUserData.authUser
+    : null
+  ), [authUserData])
 
   const router = useRouter();
 
+  const errorHandler = (typeError: string): void => {
+    switch (typeError) {
+      case 'invalid_email':
+        setEmailError('Invalid Email');
+        break;
+      case 'email_not_confirmed':
+        setEmailError('Email not confirmed');
+        break;
+      case 'invalid_password':
+        setPasswordError('Invalid password')
+        break;
+      default:
+        break;
+      }
+    };
+
   const [signIn] = useSignInMutation({
     onCompleted: res => router.push('/'),
-    onError: res => setError(res.message),
+    onError: res => errorHandler(res.message),
   });
 
   const signInHandler = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
+
+    if (passwordError || emailError) {
+      return;
+    }
+
     signIn({ variables: { args: { email, password } } });
   }
 
+  const onChangePassword = useCallback((e: React.ChangeEvent<HTMLInputElement> ) => {
+    setPassword(e.target.value);
+    setPasswordError(null)
+  }, []);
+
+  const onChangeEmail = useCallback((e: React.ChangeEvent<HTMLInputElement> ) => {
+    setEmail(e.target.value);
+    setEmailError(null)
+  }, []);
+
+  useEffect(() => {
+    if (authUser) {
+      router.push('/');
+    }
+  }, [authUser, router]);
+
     return  (
       <ThemeProvider theme={theme}>
-        <Container component="main" maxWidth="xs">
+        <Grid container component="main" sx={{ height: '100vh' }}>
           <CssBaseline />
-          <Box
+          <Grid
+            item
+            xs={false}
+            sm={4}
+            md={7}
             sx={{
-              marginTop: 8,
-              display: 'flex',
-              flexDirection: 'column',
-              alignItems: 'center',
+              backgroundImage: 'url(https://source.unsplash.com/random)',
+              backgroundRepeat: 'no-repeat',
+              backgroundColor: (t) =>
+                t.palette.mode === 'light' ? t.palette.grey[50] : t.palette.grey[900],
+              backgroundSize: 'cover',
+              backgroundPosition: 'center',
             }}
-          >
-            <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
-              <LockOutlinedIcon />
-            </Avatar>
-            <Typography component="h1" variant="h5">
-              Sign in
-            </Typography>
-            <Box component="form" onSubmit={signInHandler} noValidate sx={{ mt: 1 }}>
-              <TextField
-                margin="normal"
-                required
-                fullWidth
-                id="email"
-                value={email}
-                onChange={e => setEmail(e.target.value)}
-                label="Email Address"
-                name="email"
-                autoComplete="email"
-                autoFocus
-              />
-              <TextField
-                margin="normal"
-                required
-                fullWidth
-                name="password"
-                label="Password"
-                type="password"
-                id="password"
-                value={password}
-                onChange={e => setPassword(e.target.value)}
-                autoComplete="current-password"
-              />
-              <FormControlLabel
-                control={<Checkbox value="remember" color="primary" />}
-                label="Remember me"
-              />
-              <Button
-                type="submit"
-                fullWidth
-                variant="contained"
-                sx={{ mt: 3, mb: 2 }}
-              >
-                Sign In
-              </Button>
-
-              <Grid container>
-                <Grid item xs>
-                  <Link href="#" variant="body2">
-                    Forgot password?
-                  </Link>
+          />
+          <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
+            <Box
+              sx={{
+                my: 8,
+                mx: 4,
+                display: 'flex',
+                flexDirection: 'column',
+                alignItems: 'center',
+              }}
+            >
+              <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+                <LockOutlinedIcon />
+              </Avatar>
+              <Typography component="h1" variant="h5">
+                Sign in
+              </Typography>
+              <Box component="form" noValidate onSubmit={signInHandler} sx={{ mt: 1 }}>
+                <TextField
+                  margin="normal"
+                  required
+                  fullWidth
+                  id="email"
+                  value={email}
+                  error={!!emailError}
+                  helperText={emailError}
+                  onChange={onChangeEmail}
+                  label="Email Address"
+                  name="email"
+                  autoComplete="email"
+                  autoFocus
+                />
+                <TextField
+                  margin="normal"
+                  required
+                  fullWidth
+                  name="password"
+                  label="Password"
+                  type="password"
+                  id="password"
+                  helperText={passwordError}
+                  error={!!passwordError}
+                  value={password}
+                  onChange={onChangePassword}
+                  autoComplete="current-password"
+                />
+                <FormControlLabel
+                  control={<Checkbox value="remember" color="primary" />}
+                  label="Remember me"
+                />
+                <Button
+                  type="submit"
+                  fullWidth
+                  variant="contained"
+                  sx={{ mt: 3, mb: 2 }}
+                >
+                  Sign In
+                </Button>
+                <Grid container>
+                  <Grid item xs>
+                    <Link href="#" variant="body2">
+                      Forgot password?
+                    </Link>
+                  </Grid>
+                  <Grid item>
+                    <Link href="#" variant="body2">
+                      {"Don't have an account? Sign Up"}
+                    </Link>
+                  </Grid>
                 </Grid>
-                <Grid item>
-                  <Link href="#" variant="body2">
-                    {"Don't have an account? Sign Up"}
-                  </Link>
-                </Grid>
-              </Grid>
+                <Copyright sx={{ mt: 5 }} />
+              </Box>
             </Box>
-          </Box>
-          <Copyright sx={{ mt: 8, mb: 4 }} />
-        </Container>
+          </Grid>
+        </Grid>
       </ThemeProvider>
     );
   }
