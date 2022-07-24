2540d2f984ccf79f31d93e3caa6f9942f71908cb
Serhii Kirichenko <91411692+SerhiiKirik@users.noreply.github.com>
2022-07-24 17:28:35 +0300

    add-mui (#38)
    Co-authored-by: Serhii Kirichenko <kirik@MacBook-Pro-Serhii.local>

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,11 +1,29 @@
 import { ApolloProvider } from '@apollo/client';
 import { initApollo } from '../src/controllers/apollo/getApolloClient';
+import Head from 'next/head';
 
 export default function MyApp({ Component, pageProps }: any) {
   const client = initApollo();
 
   return (
     <ApolloProvider client={client}>
+      <Head>
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
+        />
+
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/icon?family=Material+Icons"
+        />
+
+        <title>Meet</title>
+
+        <meta name="viewport" content="initial-scale=1, width=device-width" />
+      </Head>
+
       <Component {...pageProps} />
-    </ApolloProvider>)
+    </ApolloProvider>
+  )
 }
--- frontend/pages/signIn/index.tsx
+++ frontend/pages/signIn/index.tsx
@@ -1,5 +1,3 @@
-import { ApolloProvider } from '@apollo/client';
-import { initApollo } from '../../src/controllers/apollo/getApolloClient';
 import { SignIn } from '../../src/components/SignIn/SignIn';
 
 export default function () {
