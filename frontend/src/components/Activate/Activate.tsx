38737b20835c87fe652fd4e653b0635b7d3f79e0
Serhii Kirichenko <91411692+SerhiiKirik@users.noreply.github.com>
2022-07-28 13:40:13 +0300

    [signUp flow]: mobile layout (#65)
    Co-authored-by: Serhii Kirichenko <kirik@MacBook-Pro-Serhii.local>

--- frontend/src/components/Activate/Activate.tsx
+++ frontend/src/components/Activate/Activate.tsx
@@ -40,6 +40,7 @@ export const Activate: FC = React.memo(() => {
               variant='h2'
               textAlign='center'
               mb={2}
+              className={styles.title}
             >
               {loading && (
                 'Email confirmation'
