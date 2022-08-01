49b871ffcb0deec85f50c050e4f7e8004e0593d8
Ihor Karpyn <igor82200@gmail.com>
2022-08-01 11:28:45 +0300

    remove comented code & fixed typo

--- api/src/modules/user/user.resolvers/updateUserAvatar.resolver.ts
+++ api/src/modules/user/user.resolvers/updateUserAvatar.resolver.ts
@@ -12,49 +12,14 @@ interface Options {
   };
 }
 
-// const cloudinaryUpload = async (file: FileUpload): Promise<any> => {
-//   cloudinaryBase.config({
-//     cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-//     api_key: process.env.CLOUDINARY_API_KEY,
-//     api_secret: process.env.CLOUDINARY_API_SECRET,
-//   });
-//
-//   const { createReadStream } = await file;
-//
-//   return new Promise((resolve, reject) => {
-//
-//     const stream = createReadStream();
-//
-//     const streamLoad = cloudinaryBase.uploader.upload_stream(
-//       {
-//         upload_preset: 'development',
-//         folder: 'users/3/avatar',
-//         public_id: '3',
-//       },
-//        (
-//         error: any,
-//         result: any
-//       ) => {
-//         if (result) {
-//           resolve(result);
-//         } else {
-//           reject(error);
-//         }
-//       });
-//
-//     stream.pipe(streamLoad);
-//
-//   });
-// };
-
 export const updateUserAvatarResolver: AuthResolver<Promise<User>,
   Options> = async (_, options, ctx) => {
   const { file } = options.args;
   const userRepository = new UserRepository(ctx);
-  const clodinaryService = new CloudinaryService();
+  const cloudinaryService = new CloudinaryService();
   const { authUser } = ctx;
 
-  const uploadResult = await clodinaryService.updateUseAvatar({
+  const uploadResult = await cloudinaryService.updateUseAvatar({
     userId: authUser.id,
     file,
   });
