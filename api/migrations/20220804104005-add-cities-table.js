1ae714f610facfb19154495a966ac94fa36c6f6b
Ihor Karpyn <63306833+Ihor-Karpyn@users.noreply.github.com>
2022-08-04 13:46:15 +0300

    [API]: added cities table (#105)

---
+++ api/migrations/20220804104005-add-cities-table.js
@@ -0,0 +1,40 @@
+const table = 'cities';
+const columns = {
+  name: 'name',
+  googleId: 'google_id',
+};
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable(
+      table,
+      {
+        id: {
+          type: Sequelize.INTEGER,
+          allowNull: false,
+          primaryKey: true,
+          autoIncrement: true,
+        },
+        [columns.googleId]: {
+          type: Sequelize.STRING,
+          primaryKey: true,
+          allowNull: false,
+        },
+        [columns.name]: {
+          type: Sequelize.STRING,
+          allowNull: false,
+        },
+      },
+    );
+
+    return queryInterface.addIndex(
+      table,
+      [columns.googleId],
+    );
+  },
+
+  down: async (queryInterface) => {
+    return queryInterface.dropTable(table);
+
+  },
+};
