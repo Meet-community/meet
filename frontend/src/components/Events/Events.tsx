9af7c9a94d25e449fd235496df12c3094d69e991
Serhii Kirichenko <kirik@MacBook-Pro-Serhii.local>
2022-08-04 20:29:20 +0300

    remove select from events

--- frontend/src/components/Events/Events.tsx
+++ frontend/src/components/Events/Events.tsx
@@ -1,20 +1,11 @@
-import {
-  FC, memo, useMemo, useState,
-} from 'react';
+import { FC, memo, useMemo } from 'react';
 import { useEventsQuery } from '../../controllers/graphql/generated';
 import { EventsList } from './EventsList/EventsList';
 import { useAuthUser } from '../../controllers/entities/user/useAuthUserHook';
 import { Container } from '../UI/Container/Container';
-import { GoogleSelect } from '../../ui/Selects/GoogleSelect/GoogleSelect';
-import {
-  GoogleSelectTypes,
-  PlaceType,
-} from '../../ui/Selects/GoogleSelect/GoogleSelect.typedefs';
 
 export const Events: FC = memo(() => {
   const { data: eventsData, loading: eventsLoading } = useEventsQuery();
-  const [value, setValue] = useState<PlaceType | null>(null);
-
   const authUser = useAuthUser();
 
   const events = useMemo(() => (eventsData?.events
@@ -24,15 +15,7 @@ export const Events: FC = memo(() => {
 
   return (
     <Container pageTitle="Events" isLoading={eventsLoading}>
-      <>
-        <GoogleSelect
-          type={[GoogleSelectTypes.Cities]}
-          onChange={setValue}
-          value={value}
-        />
-
-        <EventsList events={events} user={authUser} />
-      </>
+      <EventsList events={events} user={authUser} />
     </Container>
   );
 });
