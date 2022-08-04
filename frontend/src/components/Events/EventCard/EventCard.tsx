166f0f1d04065477bc42d4bb643ffd984b662948
Ihor Karpyn <63306833+Ihor-Karpyn@users.noreply.github.com>
2022-08-04 21:53:49 +0300

    [Front]: added some styles & make header fixed (#110)

--- frontend/src/components/Events/EventCard/EventCard.tsx
+++ frontend/src/components/Events/EventCard/EventCard.tsx
@@ -6,7 +6,7 @@ import {
   CardContent,
   CardMedia,
   ImageListItem,
-  ImageListItemBar,
+  ImageListItemBar, Paper,
 } from '@mui/material';
 import Typography from '@mui/material/Typography';
 import { useRouter } from 'next/router';
@@ -36,85 +36,90 @@ export const EventCard: FC<Props> = memo((props) => {
   const router = useRouter();
 
   return (
-    <Card sx={{ maxWidth: 345 }}>
-      <Box>
-        <ImageListItem sx={{ width: '100%' }}>
-          <CardMedia
-            component="img"
-            height="180"
-            image={event.logo ? event.logo : '/static/images/cards/contemplative-reptile.jpg'}
-            alt="Event Logo"
-          />
-
-          <ImageListItemBar
-            title={event.title}
-            subtitle={`@${event.creator.firstName} ${event.creator.lastName}`}
-            actionIcon={(
-              <IconButton
-                sx={{ color: 'rgba(255, 255, 255, 0.54)' }}
-                aria-label={`info about ${23}`}
-              />
-            )}
-          />
-        </ImageListItem>
-
-        <CardContent>
-          <div className={styles.creator}>
-            <Typography gutterBottom variant="h5" component="div">
-              {formatDate(event.startAt.valueOf())}
-            </Typography>
-          </div>
-
-          <Typography
-            variant="body2"
-            color="text.secondary"
-            className={styles.description}
-          >
-            {event.description}
-          </Typography>
-
-          <div className={styles.participants}>
-            {event.participants.length > 0 && (
-            <AvatarGroup max={4}>
-              {event.participants.map((person) => (
-                <Avatar
-                  alt={`${person.firstName} ${person.lastName}`}
-                  src={person.avatar || '/static/images/avatar/1.jpg'}
-                />
-              ))}
-            </AvatarGroup>
-            )}
-
-            {event.participants.length === 0 && (
-              <Typography className={styles.subtitle}>
-                <Group />
-
-                Participants
-              </Typography>
-            )}
-
-            <Typography className={styles.subtitle}>
-              {`${event.participants.length} out of ${event.capacity}`}
-            </Typography>
-          </div>
-
-          <div className={styles.groupButton}>
-            <Button
-              variant="contained"
-              onClick={() => router.push(`${ROUTES.events}/${event.id}`)}
-              endIcon={<ReadMoreIcon />}
-            >
-              Show more
-            </Button>
-
-            <SubscribeButton
-              isSubscribed={isParticipant}
-              disabled={disableSubscribe}
-              eventId={event.id}
+    <Paper
+      elevation={10}
+      sx={{ borderRadius: { xs: '0', md: '16px' } }}
+    >
+      <Card sx={{ maxWidth: 345 }}>
+        <Box>
+          <ImageListItem sx={{ width: '100%' }}>
+            <CardMedia
+              component="img"
+              height="180"
+              image={event.logo ? event.logo : '/static/images/cards/contemplative-reptile.jpg'}
+              alt="Event Logo"
             />
-          </div>
-        </CardContent>
-      </Box>
-    </Card>
+
+            <ImageListItemBar
+              title={event.title}
+              subtitle={`@${event.creator.firstName} ${event.creator.lastName}`}
+              actionIcon={(
+                <IconButton
+                  sx={{ color: 'rgba(255, 255, 255, 0.54)' }}
+                  aria-label={`info about ${23}`}
+                />
+              )}
+            />
+          </ImageListItem>
+
+          <CardContent>
+            <div className={styles.creator}>
+              <Typography gutterBottom variant="h5" component="div">
+                {formatDate(event.startAt.valueOf())}
+              </Typography>
+            </div>
+
+            <Typography
+              variant="body2"
+              color="text.secondary"
+              className={styles.description}
+            >
+              {event.description}
+            </Typography>
+
+            <div className={styles.participants}>
+              {event.participants.length > 0 && (
+                <AvatarGroup max={4}>
+                  {event.participants.map((person) => (
+                    <Avatar
+                      alt={`${person.firstName} ${person.lastName}`}
+                      src={person.avatar || '/static/images/avatar/1.jpg'}
+                    />
+                  ))}
+                </AvatarGroup>
+              )}
+
+              {event.participants.length === 0 && (
+                <Typography className={styles.subtitle}>
+                  <Group />
+
+                  Participants
+                </Typography>
+              )}
+
+              <Typography className={styles.subtitle}>
+                {`${event.participants.length} out of ${event.capacity}`}
+              </Typography>
+            </div>
+
+            <div className={styles.groupButton}>
+              <Button
+                variant="contained"
+                onClick={() => router.push(`${ROUTES.events}/${event.id}`)}
+                endIcon={<ReadMoreIcon />}
+              >
+                Show more
+              </Button>
+
+              <SubscribeButton
+                isSubscribed={isParticipant}
+                disabled={disableSubscribe}
+                eventId={event.id}
+              />
+            </div>
+          </CardContent>
+        </Box>
+      </Card>
+    </Paper>
   );
 });
