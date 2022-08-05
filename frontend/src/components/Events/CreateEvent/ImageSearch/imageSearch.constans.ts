f0e39b108248fafd012b9d01a1c978a06d813aca
Ihor Karpyn <igor82200@gmail.com>
2022-08-06 01:40:12 +0300

    [Create-event]: increase padding on mobile

--- frontend/src/components/Events/CreateEvent/ImageSearch/imageSearch.constans.ts
+++ frontend/src/components/Events/CreateEvent/ImageSearch/imageSearch.constans.ts
@@ -1,6 +1,10 @@
 export const imageSearchConfig = {
+  rowCount: {
+    m: 2,
+    pc: 2,
+  },
   gap: {
-    m: 4,
+    m: 8,
     pc: 8,
   },
   height: {
@@ -10,10 +14,14 @@ export const imageSearchConfig = {
   initialSearchQuery: 'events',
 
   get galleryHeightPc() {
-    return (this.gap.pc * 3) + (2 * this.height.pc) + 10;
+    return (this.gap.pc * this.rowCount.pc + 1)
+      + (this.rowCount.pc * this.height.pc)
+      + (this.height.pc / 3);
   },
 
   get galleryHeightM() {
-    return (this.gap.m * 3) + (2 * this.height.m) + 10;
+    return (this.gap.m * this.rowCount.m + 1)
+      + (this.rowCount.m * this.height.m)
+      + (this.height.m / 2);
   },
 };
