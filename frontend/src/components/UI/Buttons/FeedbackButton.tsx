3b31bad191b60ac9edde6c823d4fcabcd1dfc466
Serhii Kirichenko <91411692+SerhiiKirik@users.noreply.github.com>
2022-10-14 18:07:46 +0300

    [frontend]: add CreateEventButton & update FeedbackButton (#170)
    * [frontend]: add CreateEventButton & update FeedbackButton
    
    * [frontend]: remove padding
    
    Co-authored-by: Sergey Kirichenko <s.kirichenko@m-dev.org>

--- frontend/src/components/UI/Buttons/FeedbackButton.tsx
+++ frontend/src/components/UI/Buttons/FeedbackButton.tsx
@@ -1,5 +1,5 @@
 import { FC, memo } from 'react';
-import AddCommentIcon from '@mui/icons-material/AddComment';
+import RateReviewIcon from '@mui/icons-material/RateReview';
 import IconButton from '@mui/material/IconButton';
 
 interface Props {
@@ -12,11 +12,7 @@ export const FeedbackButton: FC<Props> = memo((props) => {
   return (
     <IconButton
       sx={{
-        position: 'fixed !important',
-        bottom: '24px',
-        right: '24px',
         padding: '12px',
-        zIndex: '10',
         color: '#29b6f6',
         backgroundColor: 'rgba(144, 202, 249, 0.04)',
         borderRadius: '50%',
@@ -24,7 +20,7 @@ export const FeedbackButton: FC<Props> = memo((props) => {
       color="primary"
       onClick={onClick}
     >
-      <AddCommentIcon fontSize="large" />
+      <RateReviewIcon fontSize="large" />
     </IconButton>
   );
 });
